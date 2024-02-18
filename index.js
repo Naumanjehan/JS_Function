38356622aff0afb345944ff77e951c831112b209
Naumanjehan <haristrader321@gmail.com>
2024-02-18 15:29:37 +0500

    questions

---
+++ index.js
@@ -0,0 +1,73 @@
+// let randomNumber = Math.ceil(Math.random() * 6);
+// console.log(randomNumber);
+// let flag = false;
+// for (i = 1; i <= 3; i++) {
+//   let userGess = +prompt("enter any number");
+//   if (userGess === randomNumber) {
+//     flag = true;
+//     break;
+//   }
+// }
+
+// if (flag) {
+//   console.log("you got right");
+// } else {
+//   console.log("game over");
+// }
+
+// flag variable in foor loop
+// let cleanestcity = ["pes", "isb", "lhr", "karachi"];
+// let citytocheck = prompt("enter your city name");
+
+// //flag variable
+// let cityfound = false;
+
+// for (let i = 0; i < cleanestcity.length; i++) {
+//   if (citytocheck === cleanestcity[i]) {
+//     cityfound = true;
+//     break;
+//   }
+// }
+// if (cityfound) {
+//   alert("its one of the cleanist city");
+// } else {
+//   alert("city not found");
+// }
+////////////////////////////////////////////////////////////////////////////
+// 1st Question
+// function displayWelcomeMessage() {
+//   let username = prompt("Enter Your Name");
+//   alert("Welcome:  " + username);
+// }
+// displayWelcomeMessage();
+
+////////////////////////////////////////////////////////////////////////////
+// 2nd question
+// function generateQuote() 
+// {
+//   let quotes = [
+//     "توبہ میں تاخیر کرنے سحت نقصان دہ ہوتا ہے",
+//     "اپنی ضرورتوں کو محدود کر لینا ہی بڑی دولت مندی ہے",
+//     "دعوت قبول کرنے میں امیر و غریب کا فرق مت کرو"
+//   ];
+//   let randomquotes = Math.floor(Math.random() * 3);
+//   let getquotes = quotes[randomquotes];
+//   alert(quotes[randomquotes]);
+// }
+// generateQuote();
+////////////////////////////////////////////////////////////////////////////
+// 3rd question
+// function generateRandomNumber() 
+// {
+//   let randomNumber = Math.floor(Math.random() * 100);
+//   console.log(randomNumber);
+// }
+// generateRandomNumber();
+
+//A TO Z Function
+// function generateRandomalphabit(){
+// let alphabit = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+//     let randomalphabit = Math.floor(Math.random() * 26);
+//     alert(alphabit[randomalphabit])
+//   } 
+//   generateRandomalphabit();
